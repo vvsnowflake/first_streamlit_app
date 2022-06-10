adaf193767b1efe2c6f806ab187eababc7d94eee
vvsnowflake <107159121+vvsnowflake@users.noreply.github.com>
2022-06-10 15:51:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
-streamlit,header('Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
