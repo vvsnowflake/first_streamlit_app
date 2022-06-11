111cccf2ada7bfac6707dec04830eeb9d5c984d7
vvsnowflake <107159121+vvsnowflake@users.noreply.github.com>
2022-06-11 09:20:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def insert_row_snowflake(new_fruit):
         my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
         return "Thanks for adding " + new_fruit
 
-add_my_fruit = streamlit.text_input('View Our Fruit List - Add Your Favorites!')
+add_my_fruit = streamlit.header('View Our Fruit List - Add Your Favorites!')
 if streamlit.button('Get Fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
