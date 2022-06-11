7bcede960ccc31e0a7bd4ec6eac5ade19a86f011
vvsnowflake <107159121+vvsnowflake@users.noreply.github.com>
2022-06-11 08:48:55 -0400

    add new fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,22 @@ if streamlit.button('Get Fruit Load List'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)
+        
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx_cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+
+
+#This will not work correctly, but just go with it for now
+
 #Before1 introducing if & else
 #fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 #streamlit.write('The user entered', fruit_choice)
@@ -77,9 +93,4 @@ streamlit.stop()
 
 
 
-#Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
 
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
