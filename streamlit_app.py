68c2321951292e5af11aa98a136bfd5db0169d5f
vvsnowflake <107159121+vvsnowflake@users.noreply.github.com>
2022-06-11 08:30:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+      
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -32,12 +38,14 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information:")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e:
    streamlit.error()
+    
+    
+#Before1 introducing if & else
 #fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 #streamlit.write('The user entered', fruit_choice)
 
@@ -49,6 +57,8 @@ except URLError as e:
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 #streamlit.dataframe(fruityvice_normalized)
+#End of Before1
+
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
